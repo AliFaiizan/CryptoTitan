2b073096a05c2e8b5cbfb4d01e698d5ba8bc4efe
AliFaiizan <alifaizanahmad@gmail.com>
2022-07-15 11:50:28 -0700

    store setup

---
+++ store/action/auth.action.ts
@@ -0,0 +1,95 @@
+//import { GOOGLE_API } from "../../secrets";
+//import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export const AUTHENTICATE = "AUTHENTICATE";
+export const SET_DID_TRY_AL = "SET_DID_TRY_AL";
+
+export const authenticate = (userId: any, token: any) => {
+  return { type: AUTHENTICATE, userId, token };
+};
+
+export const setDidTryAL = () => {
+  return {
+    type: SET_DID_TRY_AL,
+  };
+};
+
+export const signUp = (email: any, password: any) => {
+  return async (dispatch: any) => {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${GOOGLE_API}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password, returnSecureToken: true }),
+      }
+    );
+
+    if (!response.ok) {
+      if (!response.ok) {
+        const { error } = await response.json();
+        let message = "Email Exists";
+        if (error.message === "EMAIL_EXISTS") {
+          message = "This email already exists";
+        }
+        throw new Error(message);
+      }
+    }
+
+    const resData = await response.json();
+
+    dispatch(authenticate(resData.localId, resData.idToken));
+    const expirationDate = new Date().getTime() + +resData.expiresIn * 1000; //plus converts it into number
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+  };
+};
+
+export const login = (email: any, password: any) => {
+  return async (dispatch: any) => {
+    const response = await fetch(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${GOOGLE_API}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password, returnSecureToken: true }),
+      }
+    );
+
+    if (!response.ok) {
+      const { error } = await response.json();
+
+      let message = "Something Went Wrong";
+      if (error.message === "EMAIL_NOT_FOUND") {
+        message = "This email could not be found";
+      } else if (error.message === "INVALID_PASSWORD") {
+        message = "Passoword in Incorrect";
+      }
+      throw new Error(message);
+    }
+
+    const resData = await response.json();
+
+    dispatch(authenticate(resData.localId, resData.idToken));
+    const expirationDate = new Date().getTime() + +resData.expiresIn * 1000; //plus converts it into number
+    saveDataToStorage(resData.idToken, resData.localId, expirationDate);
+  };
+};
+
+export const saveDataToStorage = (
+  token: any,
+  userId: any,
+  expirationDate: any
+) => {
+  AsyncStorage.setItem(
+    "userData",
+    JSON.stringify({
+      token,
+      userId,
+      expirationDate,
+    })
+  );
+};
---
+++ store/reducer/auth.reducer.ts
@@ -0,0 +1,31 @@
+//import { AUTHENTICATE, SET_DID_TRY_AL } from "../actions/auth";
+
+const initialState = {
+  token: null,
+  userId: null,
+  didTryAL: false,
+};
+
+export default (state = initialState, action: any) => {
+  switch (action.type) {
+    case AUTHENTICATE:
+      return {
+        token: action.token,
+        userId: action.userId,
+        didTryAL: true,
+      };
+    case SET_DID_TRY_AL:
+      return {
+        ...state,
+        didTryAL: true,
+      };
+    // case SIGN_UP:
+    //     return{
+    //         token:action.token,
+    //         userId:action.userId
+    //     }
+
+    default:
+      return state;
+  }
+};
