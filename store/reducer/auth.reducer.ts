8466ef12b0b7ba6412fe97a6dd5b714706457c66
AliFaiizan <alifaizanahmad@gmail.com>
2022-07-17 11:39:44 -0700

    Async Storage

--- store/reducer/auth.reducer.ts
+++ store/reducer/auth.reducer.ts
@@ -7,25 +7,25 @@ const initialState = {
 };
 
 export default (state = initialState, action: any) => {
-  switch (action.type) {
-    case AUTHENTICATE:
-      return {
-        token: action.token,
-        userId: action.userId,
-        didTryAL: true,
-      };
-    case SET_DID_TRY_AL:
-      return {
-        ...state,
-        didTryAL: true,
-      };
-    // case SIGN_UP:
-    //     return{
-    //         token:action.token,
-    //         userId:action.userId
-    //     }
+  // switch (action.type) {
+  //   case AUTHENTICATE:
+  //     return {
+  //       token: action.token,
+  //       userId: action.userId,
+  //       didTryAL: true,
+  //     };
+  //   case SET_DID_TRY_AL:
+  //     return {
+  //       ...state,
+  //       didTryAL: true,
+  //     };
+  //   // case SIGN_UP:
+  //   //     return{
+  //   //         token:action.token,
+  //   //         userId:action.userId
+  //   //     }
 
-    default:
-      return state;
-  }
+  //   default:
+  //     return state;
+  // }
 };
