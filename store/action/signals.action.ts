9246774d131c71b3f13b7d04ffa3c5230c3213ee
AliFaiizan <alifaizanahmad@gmail.com>
2022-10-28 23:49:10 +0500

    signal polst action

--- store/action/signals.action.ts
+++ store/action/signals.action.ts
@@ -29,3 +29,30 @@ export const getSignals:Function = () => {
   };
 }
 
+//need work to be done
+
+export const postSignals: Function = () => {
+  return async (dispatch: Function) => {
+    try {
+      const response = await fetch(`http://192.168.1.20:4000/signal`,{
+        method:'POST',
+      });
+
+      if (!response.ok) {
+        const {error} = await response.json();
+        let message;
+        if (error.message === 'NO_SIGNALS') {
+          message = 'There are no signals';
+        }
+        throw new Error(message);
+      }
+
+      const signalArray = await response.json();
+
+      dispatch({type: GET_SIGNALS, signals: signalArray});
+    } catch (err) {
+      console.log('there is some error in fetching a signal', err);
+    }
+  };
+};
+
